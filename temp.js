05cc866bc73444aa1a160f81ddc98cdfea713dc4
LeoGiaco <51151976+LeoGiaco@users.noreply.github.com>
2019-06-11 15:40:37 +0200

    php

--- temp.js
+++ temp.js
@@ -13,5 +13,48 @@ $(()=>{
             $("ul").empty();
             $("ul").append(html.slice(0,html.indexOf('<!')));
         });
+
+        $('.modify').remove();
+        var formData = new FormData();
+        formData.append('num',$(e.target).val());
+        $.ajax({
+            url: "bottone.php",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function(html){
+            $("ul").empty();
+            $("ul").append(html.slice(0,html.indexOf('<!')));
+        });
+        
+        $('.create').remove();
+        var formData = new FormData();
+        formData.append('num',$(e.target).val());
+        $.ajax({
+            url: "bottone.php",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function(html){
+            $("ul").empty();
+            $("ul").append(html.slice(0,html.indexOf('<!')));
+        });
+        
+        $('.delete').remove();
+        var formData = new FormData();
+        formData.append('num',$(e.target).val());
+        formData.append('action',"delete");
+        $.ajax({
+            url: "bottone.php",
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function(html){
+            $("ul").empty();
+            $("ul").append(html.slice(0,html.indexOf('<!')));
+        });
     });
 })
